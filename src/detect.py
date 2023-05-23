367a72958b69a5079cca99d5aeec91f1e0c8d4ab
littlebutt <luogan199686@gmail.com>
2023-05-23 19:32:29 +0800

    chore: make images

--- src/detect.py
+++ src/detect.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    model = YOLO('../runs/detect/train9/weights/best.pt')
+    model = YOLO('../runs/detect/train10/weights/best.pt')
     results = model('../data/detect/2.jpg')
     for result in results:
         print(result)
